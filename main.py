5d023ee3160acea4bb5a20589c72155805637870
lpawlak1 <lpawlak@student.agh.edu.pl>
2021-12-04 01:29:44 +0100

    Working backend :)

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, File, UploadFile
 from typing import List, Dict, Optional
 from fastapi.middleware.cors import CORSMiddleware
 from starlette.responses import RedirectResponse
-from data_preprocessing import preprocess
+from data_processing import preprocess
 import pandas as pd
 
 app = FastAPI()
